823b61a82a423fd688b1295ad056291208a56559
Miguel Lyra <151979534+miguelfordev@users.noreply.github.com>
2025-08-24 20:03:28 -0300

    add interface printer/scanner and modify class

--- src/main/java/br/com/mariojp/solid/isp/SimplePrinter.java
+++ src/main/java/br/com/mariojp/solid/isp/SimplePrinter.java
@@ -1,6 +1,14 @@
 package br.com.mariojp.solid.isp;
 
 public class SimplePrinter implements MultiFunctionDevice {
-    @Override public void print(String content){ /* imprime */ }
-    @Override public void scan(String targetFile){ throw new UnsupportedOperationException("Sem scanner"); }
+    @Override 
+    public void print(String content){ 
+    	/* imprime */
+    	System.out.println("Teste de impressão.");
+    }
+    
+    @Override 
+    public void scan(String targetFile){ 
+    	throw new UnsupportedOperationException("Sem scanner"); 
+    }
 }
